48d39f5ff46ebd55144c0613941f9868f59f0460
VasiliyLysov <63718467+VasiliyLysov@users.noreply.github.com>
2020-04-15 14:17:14 +0300

    Add files via upload

---
+++ java-zolotaya/src/main/java/com/WRT/java/zolotaya/JavaZolotayaApplication.java
@@ -0,0 +1,13 @@
+package com.WRT.java.zolotaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaZolotayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaZolotayaApplication.class, args);
+	}
+
+}
---
+++ java-zolotaya/src/test/java/com/WRT/java/zolotaya/JavaZolotayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WRT.java.zolotaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaZolotayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
